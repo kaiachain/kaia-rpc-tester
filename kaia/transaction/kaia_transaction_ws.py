74c03868ff3e292338d77d7294bc1d19f4cdb27d
Toshimasa Nasu <toshimasa_nasu@hotmail.com>
2025-07-09 09:46:40 +0900

    Update kaia/transaction/kaia_transaction_ws.py

--- kaia/transaction/kaia_transaction_ws.py
+++ kaia/transaction/kaia_transaction_ws.py
@@ -1601,7 +1601,7 @@ class TestKaiaNamespaceTransactionWS(unittest.TestCase):
         ]
         _, error = Utils.call_ws(self.endpoint, method, params, self.log_path)
         #Utils.check_error(self, "GasRequiredExceedsAllowance", error)
-        self.assertIsNone(error) # The way "arg.Gas" is handled is different
+        self.assertIsNone(error) # "arg.Gas" is handled differently
 
     def test_kaia_estimateGas_error_evm_revert_message(self):
         method = f"{self.ns}_estimateGas"
